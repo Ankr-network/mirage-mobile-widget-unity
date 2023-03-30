672f4643d7ef91a90b55788bede7a8742fd76535
Anton Vikharev <anton.vikharev@decentraland.org>
2023-03-30 17:31:38 +0300

    import statement missing

--- Mirage-Android-WebView-Plugin/app/src/main/java/com/mirage/miragewebviewandroidplugin/MirageWebViewActivity.java
+++ Mirage-Android-WebView-Plugin/app/src/main/java/com/mirage/miragewebviewandroidplugin/MirageWebViewActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.webkit.WebView;
 import android.widget.ImageButton;
